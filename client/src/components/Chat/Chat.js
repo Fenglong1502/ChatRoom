b566b464de212ee18fdb1d065d119efc82807a7e
fenglong_ng <fenglong1502@hotmail.com>
2020-07-30 20:10:21 +0800

    added socketIO

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({location}) => {
+    const [username, setUsername] = useState('');
+    const [roomName, setRoomName] = useState('');
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(()=> {
+        const {username, roomName} = queryString.parse(location.search);
+        
+        socket = io(ENDPOINT);
+
+        setUsername(username);
+        setRoomName(roomName);
+
+        socket.emit('join', { username, roomName }, () => {
+
+        }); 
+
+        return () => {
+            socket.emit('disconnect');
+
+            socket.off();
+        }
+    }, [ENDPOINT, location.search])
     return (
         <div>
             <h1>Chat</h1>
--- client/src/components/JoinRoom/JoinRoom.js
+++ client/src/components/JoinRoom/JoinRoom.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import './JoinRoom.css';
 
 const JoinRoom = () => {
+    const [username, setUsername] = useState('');
+    const [roomName, setRoomName] = useState('');
+
     return (
-        <div>
-            <h1>Join Room</h1>
+        <div className="joinRoomOuterContainer">
+            <div className="joinRoomContainer">
+                <h1 className="heading">Join Room</h1>
+                <div>
+                    <input placeholder="Enter an username" className="joinPageInput" type="text" onChange={(event) => setUsername(event.target.value)} />
+                </div>
+                <div>
+                    <input placeholder="Enter a room name" className="joinPageInput" type="text" onChange={(event) => setRoomName(event.target.value)} />
+                </div>
+                <Link onClick={event => (!username || !roomName) ? event.preventDefault() : null} to={`./chat?username=${username}&roomName=${roomName}`}>
+                    <button className="joinButton" type="submit"> Join Room </button>
+                </Link>
+            </div>
         </div>
     )
 }
